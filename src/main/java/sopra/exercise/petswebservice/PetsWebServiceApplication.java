3062012bb31762876e53c0e3a2c1cdf1e1895567
emil <emil@dekeyser.xyz>
2022-06-05 13:48:43 +0200

    add scaffolding + deps

---
+++ src/main/java/sopra/exercise/petswebservice/PetsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package sopra.exercise.petswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopra/exercise/petswebservice/PetsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sopra.exercise.petswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
